5f982acbe7415f8ea024454d14df36de2680bbf7
Akash <65025242+psychoAKASH@users.noreply.github.com>
2022-12-25 01:52:28 +0530

    Update settings.py

--- CarPricePrediction/CarPricePrediction/settings.py
+++ CarPricePrediction/CarPricePrediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rx)x$s0a=yvjyy(=--qgtt6@l4lmvbiow#w&^g7ug%4#5uzgf2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['carpricepredictor-production.up.railway.app']
 
 
 # Application definition
